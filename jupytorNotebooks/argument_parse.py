7d5dbb86e47006a49835a85c30d08574c969f364
YJOh <trimurti@naver.com>
2021-12-22 20:03:39 +0900

    wed

--- jupytorNotebooks/argument_parse.py
+++ jupytorNotebooks/argument_parse.py
@@ -5,7 +5,7 @@ def _parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--from_date",
-        help="starting date",  # python boilerplate.py -h/--help
+        help="starting date",  # python argument_parse.py -h/--help
         type=str,
         default="2020.01.01"
     )
@@ -22,10 +22,10 @@ def hello():
     print('Hello world to Python programming')
 
 if __name__ == '__main__':
-    hello()
 #     print(sys.argv)
-#     print(sys.argv[1:])
-    
+#     print(sys.argv[1:]) 
     args = _parse_arguments(sys.argv[1:])[0]
     print(args)
     print(args.from_date, args.to_date)
+    
+    hello()
